128fad8809fb204611523172babab65c2b17e868
Ivan Gamboa Ultreras <gamboita9@hotmail.com>
2023-07-06 11:32:01 -0600

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -4,24 +4,10 @@ if (
   url.startsWith("https://www3.animeflv.net/perfil/") &&
   url.split("/").length === 5
 ) {
-  const section = document.createElement("section");
-  // Section Title
-  section.className = "WdgtCn";
-  const sectionTitleTop = document.createElement("div");
-  sectionTitleTop.className = "Top";
-  const sectionTitle = document.createElement("div");
-  sectionTitle.className = "Title";
-  sectionTitle.textContent = "Animes vistos";
-  sectionTitleTop.appendChild(sectionTitle);
-  section.appendChild(sectionTitleTop);
-
-  //Section List
-
-  //Section bottom
-  const button = document.createElement("a");
-  button.href = `/perfil/${url.split("/")[4]}/vistos`;
-  button.classList.add("Button", "StylC", "ShwMr", "Alt");
-  button.textContent = "Ver todos";
-  section.appendChild(button);
-  document.getElementsByClassName("Main").item(0).prepend(section);
+  $(".Main").prepend(`
+  	<section class="WdgtCn">
+		<div class="Top"><div class="Title">Animes ya vistos</div></div>
+		<a href="/perfil/${url.split("/")[4]}/vistos" class="Button StylC ShwMr Alt">Ver todos</a>
+	</section>
+  `);
 }
